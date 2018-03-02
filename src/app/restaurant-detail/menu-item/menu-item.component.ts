d7ad2530372ae530089a60b26a7bc92c1ddc7e10
Tiago Boeing <tiagoboeing@bol.com.br>
2018-03-02 16:37:01 -0300

    Menu com comidas do restaurante e saída no console de item do cart

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { MenuItem } from './menu-item.model'
 
 @Component({
   selector: 'mt-menu-item',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuItemComponent implements OnInit {
 
+  //quer dizer que o parent vai informar os dados
+  @Input() menuItem: MenuItem
+  @Output() add = new EventEmitter()
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  emitAddEvent(){
+    this.add.emit(this.menuItem)
   }
 
 }
