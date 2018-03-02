6b422194eefb069c3ef0589cccb42421045e5a00
Tiago Boeing <tiagoboeing@bol.com.br>
2018-03-02 17:23:10 -0300

    Add to cart em funcionamento

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -13,12 +13,26 @@ export class ShoppingCartComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  
   items(): any[]{
     return this.shoppingCartService.items;
   }
 
   total(): number{
     return this.shoppingCartService.total()
+  }
+
+  clear(){
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(item: any){
+    this.shoppingCartService.removeItem(item)
+  }
+
+  addItem(item: any){
+    this.shoppingCartService.addItem(item)
   }
 
 }
