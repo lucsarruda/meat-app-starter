119b96e100d7da15d921e023fb4a937faabbd0f9
Tiago Boeing <tiagoboeing@bol.com.br>
2018-03-19 15:17:42 -0300

    Reactive forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Router } from '@angular/router';
 
@@ -13,6 +14,9 @@ import { Order, OrderItem } from './order.model';
 })
 export class OrderComponent implements OnInit {
 
+  orderForm: FormGroup
+
+
   // em aplicação real, informado no back-end
   delivery: number = 8
 
@@ -23,9 +27,23 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão refeição', value: 'REF' }
   ]
 
-  constructor(private orderService: OrderService, private router: Router) { }
+  constructor(private orderService: OrderService, 
+              private router: Router,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    //trabalhando com form builder
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control(''),
+      email: this.formBuilder.control(''),
+      emailConfirmation: this.formBuilder.control(''),
+      address: this.formBuilder.control(''),
+      number: this.formBuilder.control(''),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('')
+    })
+    
   }
 
   itemsValue(): number {
