ceb22e033a8b9a192b61bc92f52faf7ae59045c9
Tiago Boeing <tiagoboeing@bol.com.br>
2018-04-13 17:09:20 -0300

    Animations e snackbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,11 +10,12 @@ import { RatingComponent } from './rating/rating.component'
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
 import { RestaurantsService } from "../restaurants/restaurants.service";
 import { OrderService } from "../order/order.service";
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 
 @NgModule({
-    declarations: [InputComponent, RadioComponent, RatingComponent],
+    declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
-    exports: [InputComponent, RadioComponent,
+    exports: [InputComponent, RadioComponent, SnackbarComponent,
             RatingComponent, CommonModule,
             FormsModule, ReactiveFormsModule]
 })
