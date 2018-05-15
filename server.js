ac15440405e829d3dfe4c450a6cbd8ba147c9927
Tiago Boeing <tiagoboeing@bol.com.br>
2018-05-15 16:33:30 -0300

    Json server test Heroku

--- server.js
+++ server.js
@@ -14,3 +14,20 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
+
+var jsonServer = require('json-server')
+var server = jsonServer.create()
+var db = {
+  posts: [],
+  comments: []
+}
+var router = jsonServer.router(db)
+var middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, function () {
+  console.log('JSON Server is running')
+})
