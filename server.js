0c9466e0e10161f2192c52e37dfbdf5304b73b3b
Tiago Boeing <tiagoboeing@bol.com.br>
2018-05-21 12:06:06 -0300

    Correção de problemas de build no Heroku

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ app.listen(process.env.PORT || 8080);
 
 
 // server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const path = require('path')
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const path = require('path')
+// const router = jsonServer.router(path.join(__dirname, 'db.json'))
+// const middlewares = jsonServer.defaults()
  
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+// server.use(middlewares)
+// server.use(router)
+// server.listen(3000, () => {
+//   console.log('JSON Server is running')
+// })
