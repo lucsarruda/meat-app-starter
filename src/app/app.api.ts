6cf4db6329c8ebc88d4a1d75c1ba098ee32fe540
Tiago Boeing <tiagoboeing@bol.com.br>
2018-07-30 19:05:41 -0300

    Update app.api.ts

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -2,5 +2,5 @@
 
 // export const MEAT_API = environment.api
 
-// export const MEAT_API = 'https://tiagoboeing-meat-api.herokuapp.com';
-export const MEAT_API = 'https://localhost:3001';
+export const MEAT_API = 'https://tiagoboeing-meat-api.herokuapp.com';
+// export const MEAT_API = 'https://localhost:3001';
