e1ddfb54200cb33308096804506569c66f139400
Tiago Boeing <tiagoboeing@bol.com.br>
2018-11-13 00:10:42 -0200

    Utilizando zonas

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,4 +1,4 @@
-import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/observable/throw'
@@ -8,27 +8,31 @@ import { LoginService } from './security/login/login.service';
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
 
-    constructor(private ns: NotificationService, private injector: Injector) {
-        super()
-    }
+  constructor(private ns: NotificationService,
+    private injector: Injector,
+    private zone: NgZone) {
+    super()
+  }
 
-    handleError(errorResponse: HttpErrorResponse | any) {
-        if (errorResponse instanceof HttpErrorResponse) {
-            const message = errorResponse.error.message
-            switch (errorResponse.status) {
-                case 401:
-                    this.injector.get(LoginService).handleLogin()
-                    break;
+  handleError(errorResponse: HttpErrorResponse | any) {
+    if (errorResponse instanceof HttpErrorResponse) {
+      const message = errorResponse.error.message
+      this.zone.run(() => {
+        switch (errorResponse.status) {
+          case 401:
+            this.injector.get(LoginService).handleLogin()
+            break;
 
-                case 403:
-                    this.ns.notify(message || 'Não autorizado.')
-                    break;
+          case 403:
+            this.ns.notify(message || 'Não autorizado.')
+            break;
 
-                case 404:
-                    this.ns.notify(message || 'Não encontrado.')
-                    break;
-            }
+          case 404:
+            this.ns.notify(message || 'Não encontrado.')
+            break;
         }
-        super.handleError(errorResponse)
+      });
     }
+    super.handleError(errorResponse)
+  }
 }
